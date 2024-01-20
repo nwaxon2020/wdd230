c03f4d3fdf527ae306c2837033078ae736303693
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2024-01-20 11:11:03 +0100

    dark mode fixed

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -11,3 +11,9 @@ hambug.addEventListener("click", () => {
     }
 });
 
+const darkMode = document.querySelector("#dark-mode");
+
+darkMode.addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("dark");
+});
+
