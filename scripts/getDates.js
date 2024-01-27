1fbeee3749bae2d1a29f9e32898252d55dad6eea
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2024-01-27 19:02:09 +0100

    site updated

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,17 @@ const lastModif = document.getElementById("lastModified")
 let oLastModif = new Date(document.lastModified);
 lastModif.innerText = "Last Modified: " + oLastModif;
 lastModif.style.color = "#ffbe0b";
+
+const visit = document.querySelector(".visit");
+
+let numberVisit = Number(window.localStorage.getItem("numVisited")) || 0;
+
+if (numberVisit !== 0) {
+    visit.textContent = `Visited: ${numberVisit}`;
+} else {
+    visit.textContent = "This is your first visit, Welcome";
+}
+
+numberVisit++;
+
+localStorage.setItem("numVisited", numberVisit);
