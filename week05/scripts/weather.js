295233ca2e5a84bb543c5626582b0bd7955b27ad
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2024-02-10 21:17:26 +0100

    weather done

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=2a7dbed74d65853479d15c70065ab709"
+
+async function apiFetch() {
+    try {
+        let response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json(); // Make sure to await response.json() to get the parsed JSON data
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
